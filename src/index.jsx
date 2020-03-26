773fb2065ec68276d6892e3a6df9e8c4ddfc04e8
canozo <canwoahzo@gmail.com>
2020-03-26 01:30:17 -0600

    Commit inicial

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+ReactDOM.render(
+  <div>React App</div>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  devServer: {
+    contentBase: `${__dirname}/public`,
+    compress: true,
+    port: 3000
+  },
+  entry: './src/index.jsx',
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader'
+        },
+        test: /\.jsx?$/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
